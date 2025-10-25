bc331e6be578501c02f8b978afc09c2f00c24f25
Lex Camplin <lex_camplin@sfu.ca>
2025-10-25 10:44:55 -0700

    Created the core package, Game class, and empty methods

---
+++ src/main/java/game/core/Game.java
@@ -0,0 +1,41 @@
+package game.core;
+
+public class Game {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public void tick(){
+
+    }
+
+    public void resolveCollisions(){
+
+    }
+
+    public boolean checkWin(){
+
+    }
+
+    public boolean checkLose(){
+
+    }
+
+    public void end(){
+
+
+    }
+
+    public void addReward(){
+
+    }
+
+    public void removeReward(){
+
+    }
+
+    public void shouldRespawn(){
+        
+    }
+}
