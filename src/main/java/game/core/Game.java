ef6bc7ca477c5304606ff640335b9a894080bd14
Lex Camplin <lex_camplin@sfu.ca>
2025-10-25 12:18:18 -0700

    Added the fields to the Game class, removed main() and added start()

--- src/main/java/game/core/Game.java
+++ src/main/java/game/core/Game.java
@@ -1,8 +1,37 @@
 package game.core;
 
+import java.time.Duration;
+import java.util.List;
+
+import game.entity.Enemy;
+import game.entity.Player;
+import game.map.Map;
+
 public class Game {
-    
-    public static void main(String[] args) {
+    private int score;
+    private boolean isGameOver;
+    private Duration elapsedTime;
+    private int basicToCollect;
+    private int basicCollected;
+    private Map map;
+    private Player player;
+    private List<Enemy> enemies;
+    private List<Reward> rewards;
+
+    public Game(int score, boolean isGameOver, Duration elapsedTime, int basicToCollect, int basicCollected, Map map,
+            Player player, List<Enemy> enemies, List<Reward> rewards) {
+        this.score = score;
+        this.isGameOver = isGameOver;
+        this.elapsedTime = elapsedTime;
+        this.basicToCollect = basicToCollect;
+        this.basicCollected = basicCollected;
+        this.map = map;
+        this.player = player;
+        this.enemies = enemies;
+        this.rewards = rewards;
+    }
+
+    public void start() {
         
     }
 
@@ -36,6 +65,6 @@ public class Game {
     }
 
     public void shouldRespawn(){
-        
+
     }
 }
